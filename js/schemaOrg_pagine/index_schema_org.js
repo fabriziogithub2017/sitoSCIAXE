9bc92223f601a4dc6dd845ee7639fa3a9d233909
fabrizio sciascia <sciax@virgilio.it>
2018-08-20 18:52:40 +0200

    javascript e webconfig modificati

--- js/schemaOrg_pagine/index_schema_org.js
+++ js/schemaOrg_pagine/index_schema_org.js
@@ -1,22 +1,32 @@
 ﻿//Schema.org Organization
-{
-    "@context": "http://schema.org",
-        "@type": "Organization",
-            "url": "http://www.sciaxe.it/",
-                "logo": "http://www.sciaxe.it/jmg/logo-sciaxe.png",
-                    "contactPoint": [{
-                        "@type": "ContactPoint",
-                        "telephone": "+39 0113975398",
-                        "contactType": "customer service"
-                    }],
-                        "address": {
-        "@type": "PostalAddress",
-            "addressLocality": "Grugliasco",
-                "addressRegion": "To",
-                    "streetAddress": "Strada del Potrone 10",
-                        "postalCode": "10095"
-    },
-    "description": "Serigrafia e ricamo su abbigliamento aziendale.Penne e gadget personalizzati.Produzione di timbri.",
-        "name": "Sciax'è snc",
-            "telephone": "+39 0113975398"
+var riferimento = document.getElementById("Schema.org").title;
+switch (riferimento) {
+    case "index":
+        document.getElementById("Schema.org").innerHTML = "{" +
+            "'@context': 'http://schema.org'," +
+            "'@type': 'Organization'," +
+            "'url': 'http://www.sciaxe.it/'," +
+            "'logo': 'http://www.sciaxe.it/jmg/logo-sciaxe.png'," +
+            "'contactPoint': [{'contactPoint': [{" +
+            "'@type': 'ContactPoint'," +
+            "'telephone': '+39 0113975398'," +
+            "'contactType': 'customer service'" +
+            "}]," +
+            "'address': {" +
+            "'@type': 'PostalAddress'," +
+            "'addressLocality': 'Grugliasco'," +
+            "'addressRegion': 'To'," +
+            "'streetAddress': 'Strada del Potrone 10'," +
+            "'postalCode': '10095'" +
+            "}," +
+            "'description': 'Serigrafia e ricamo su abbigliamento aziendale.Penne e gadget personalizzati.Produzione di timbri.'," +
+            "'name': 'Sciaxè snc'," +
+            "'telephone': '+39 0113975398'" +
+            "} '@type': 'ContactPoint'," +
+            "'telephone': '+39 0113975398', " +
+            "'contactType': 'customer service'" +
+            "}], ";
+        break;
+    default:
+        var e;
 }
