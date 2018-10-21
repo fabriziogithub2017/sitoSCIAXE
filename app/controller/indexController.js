96c29bbd20ddaec1d9f09fbf5d3eaaa74063c2ba
Fabrizio <Fabrizio@FABRIZIO-PC>
2018-10-21 20:32:43 +0200

    varie

--- app/controller/indexController.js
+++ app/controller/indexController.js
@@ -97,7 +97,6 @@
             'descrizione': 'Portachiavi in alluminio, incisione a laser o stampa a colori del tuo logo, dimensioni: 2, 6 x 9 cm. Peso: solo 16 gr. con scatola singola'
         },
 
-
         {
             'ahref': '../Categorie/Gadget-personalizzati.html',
             'imgSrc': '../../jmg/immaginiVetrina/bracciali-in-silicone.jpg', 'imgAlt': 'braccialetti in silicone personalizzati',
@@ -116,27 +115,28 @@
 
         {
             'ahref': '../Categorie/Borse-Zaini-personalizzati.html',
-            'imgSrc': '../../jmg/immaginiVetrina/cartella-porta-documenti-personalizzata.jpg', 'imgAlt': 'cartella porta documenti personalizzata a 1 colore',
+            'imgSrc': '../../jmg/immaginiVetrina/cartella-porta-documenti.jpg', 'imgAlt': 'cartella porta documenti personalizzata a 1 colore',
             'prezzoVetrina': 'da € 1,60',
             'titolo': 'Cartella porta documenti',
-            'descrizione': 'Pratica cartella porta documento in nylon, robusta in tessuto pesante'
+            'descrizione': 'Pratica cartella porta documenti, robusta in tessuto pesante personalizzata a 1 colore'
         },
 
         {
-            'ahref': '../Categorie/Block-Notes-personalizzati-a-più-colori.html',
-            'imgSrc': '../../jmg/immaginiVetrina/block notes.jpg', 'imgAlt': 'block notes personalizzati',
-            'prezzoVetrina': 'da € 1,60',
-            'titolo': 'Block notes personalizzati',
-            'descrizione': 'Block notes personalizzato con il tuo logo aziendale a righe o a quadretti. Formati A4, A5, A6'
+            'ahref': '../Categorie/Gadget-personalizzati.html',
+            'imgSrc': '../../jmg/immaginiVetrina/bracciali-in-silicone-in-rilievo.jpg', 'imgAlt': 'braccialetti in silicone personalizzati in rilievo con lo stesso colore',
+            'prezzoVetrina': 'da € 0,78',
+            'titolo': 'Braccialetti in silicone',
+            'descrizione': 'Braccialetti in silicone a più colori, personalizzati in rilevo con lo stesso colore del braccialetto'
         },
 
         {
-            'ahref': '../Categorie/Timbri-datari.html',
-            'imgSrc': '../../jmg/immaginiVetrina/Timbri e datari.jpg', 'imgAlt': 'timbri e datari',
-            'prezzoVetrina': 'da € 10',
-            'titolo': 'Timbri autoinchiostrati e datari',
-            'descrizione': 'Timbri autoinchiostrati. Datari automatici e manuali. Tamponi a più colori.'
-        },
+            'ahref': '../Categorie/Gadget-personalizzati.html',
+            'imgSrc': '../../jmg/immaginiVetrina/bracciali-in-silicone-incisi.jpg', 'imgAlt': 'braccialetti in silicone personalizzati incisi con lo stesso colore',
+            'prezzoVetrina': 'da € 0,75',
+            'titolo': 'Braccialetti in silicone',
+            'descrizione': 'Braccialetti in silicone a più colori, personalizzati con incisione con lo stesso colore del braccialetto'
+
+        }        
 
     ]
     
