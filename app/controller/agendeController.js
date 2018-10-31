20aec5e682ead720142e4c137e684998a8449deb
fabrizio sciascia <sciax@virgilio.it>
2018-10-31 08:43:36 +0100

    varie

--- app/controller/agendeController.js
+++ app/controller/agendeController.js
@@ -16,21 +16,52 @@
     }
 
     vm.categoria = [{
-        'titolo': 'Agenda 2019 giornaliera', 'imgSrc': '../../jmg/jmgAgende/agenda-2019-giornaliera-21x15.jpg', 'imgAlt': 'Agenda giornaliera 2019 personalizzata sulla copertina',
+        'titolo': 'Agendina giornaliera', 'imgSrc': '../../jmg/jmgAgende/agendina-giornaliera.jpg', 'imgAlt': 'Agendina giornaliera 2019 personalizzata sulla copertina',
         'caratteristiche': [
-            'F.to agenda cm 15x21',
-            'carta bianca ',
-            'a righe',
-            'edizione multilingue: IT – GB – FR – DE – ES – PT',
-            'inserti informativi e rubrica telefonica',
-            'inserto cartina geografica',
-            'angolo segnapagine perforato ',
+            'F.to agenda cm 9 x 15',
+            'carta avorio a righe ',
+            '352 pagine',
+            'sab.e dom.abbinati',
+            'edizione multilingue',
+            'inserti informativi',
+            '16 pag.removibili perforate',
+            'quadranti con angoli tondi ',
             'laccetto segnapagine ',
-            '8 colori disponibili',
-            'materiale poliuretano termovirante'],
-        'prezzo': 'da € 2,90 cad. +iva'
+            '5 colori disponibili',
+            'tasca posteriore interna',
+            'chiusura con elastico'],
+        'prezzo': 'da € 3,50 cad. +iva'
     },
-    
+
+        {
+            'titolo': 'Agenda 2019 giornaliera', 'imgSrc': '../../jmg/jmgAgende/agenda-2019-giornaliera-21x15.jpg', 'imgAlt': 'Agenda giornaliera 2019 personalizzata sulla copertina',
+            'caratteristiche': [
+                'F.to agenda cm 15x21',
+                'carta bianca ',
+                'a righe',
+                'edizione multilingue: IT – GB – FR – DE – ES – PT',
+                'inserti informativi e rubrica telefonica',
+                'inserto cartina geografica',
+                'angolo segnapagine perforato ',
+                'laccetto segnapagine ',
+                '8 colori disponibili',
+                'materiale poliuretano termovirante'],
+            'prezzo': 'da € 2,90 cad. +iva'
+        },
+        {
+            'titolo': 'Notes a righe', 'imgSrc': '../../jmg/jmgAgende/notes-a-righe-con-elastico.jpg', 'imgAlt': 'Notes a righe personalizzato sulla copertina',
+            'caratteristiche': [
+                'F.to agenda cm 9 x 14',
+                'carta bianca a righe ',
+                '160 pagine',
+                'quadranti con angoli tondi ',
+                'laccetto segnapagine ',
+                '5 colori disponibili',
+                'chiusura con elastico'],
+            'prezzo': 'da € 1,50 cad. +iva'
+            
+        }
+
     ]
-    
+
 })
